7aadb42ddab47a4046fb353d3fbf9f60253abf5a
Isaac Kelly <isaacmkelly0@gmail.com>
2019-07-18 20:15:46 -0500

    removed operation class from = button, started script file

---
+++ script.js
@@ -0,0 +1,8 @@
+var numbers = [];
+var num = '';
+const numButton = document.querySelectorAll('.number');
+const opButton = document.querySelectorAll('.operation');
+const equals = document.querySelector('#equals');
+function operation(){
+
+};
