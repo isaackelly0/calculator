f16a94ee9e365a9e4c0abc3587a09a7813e45b2d
Isaac Kelly <isaacmkelly0@gmail.com>
2019-07-18 21:37:44 -0500

    number string can now fetch from inner html of the button pressed

--- script.js
+++ script.js
@@ -3,19 +3,27 @@ var num = '';
 const numButton = document.querySelectorAll('.number');
 const opButton = document.querySelectorAll('.operation');
 const equals = document.querySelector('#equals');
-function operation(){
+function operation(arr){
 
 };
 function display(){
 
 };
+//add numbers to the list of operations
 numButton.forEach(function(elem){
   elem.addEventListener('click', function(){
-    alert('it worked');//it doesnt tho
+    num += elem.innerHTML; //add numbers to string
   })
 })
+//add next operation and ready next number for the list
 opButton.forEach(function(elem){
   elem.addEventListener('click', function(){
-    alert('this also works');
+    numbers.push(num);
+    numbers.push(elem.innerHTML);
+    num = '';
   })
+})
+//call operation function to evaluate list of numbers
+equals.addEventListener('click', function(){
+  operation(numbers);
 })
