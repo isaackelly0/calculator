84eb9a963a5c1c8dae6cc76c913e5dfd723639fc
Isaac Kelly <isaacmkelly0@gmail.com>
2019-07-18 20:32:38 -0500

    some stylistic changes, added event listeners to buttons

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ const equals = document.querySelector('#equals');
 function operation(){
 
 };
+function display(){
+
+};
+numButton.forEach(function(elem){
+  elem.addEventListener('click', function(){
+    alert('it worked');//it doesnt tho
+  })
+})
+opButton.forEach(function(elem){
+  elem.addEventListener('click', function(){
+    alert('this also works');
+  })
+})
